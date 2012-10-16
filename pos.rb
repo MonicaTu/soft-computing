b4b6bccf2c84d02371ae6e9f05e5d89e9a98d9b6
Monica Tu <monica.service@gmail.com>
2012-10-16 16:08:36 +0800

    Require sphere.rb & rastrigin.rb.

--- pos.rb
+++ pos.rb
@@ -1,36 +1,32 @@
-c1 = 1
-c2 = 1
+#require "./sphere.rb"
+require "./rastrigin.rb"
+
+c1 = 2
+c2 = 2
 r1 = rand
 r2 = rand
-n = 100
+n = 50
 t = 100
 v = [0, 0]
-max = 100
-#max = 4
+max = 100 # sphere
+#max = 4 # rastrigin 
+
+puts "r1:#{r1} r2:#{r2}"
 
 PI = 3.141592654
 
-z_gbest = 100 * 100 + 100 * 100
 pos_pbest = [0, 0]
 pos_curr = [0, 0]
 
 particle = Array.new(n)
 
-def sphere(x, y)
-   result = x**2 + y**2 
-   return result
-end
-
-def rastrigin(x, y)
-   result = (x**2-10*Math.cos(2*PI*x)+10) + (y**2-10*Math.cos(2*PI*y)+10) 
-   return result
-end
-
 for i in 1..n
     x = rand(max*2)-max
     y = rand(max*2)-max
-    z = sphere(x, y) 
-#    z = rastrigin(x, y) 
+
+    z_gbest = fn(max, max) 
+
+    z = fn(x, y) 
     pos_new = [x, y]
     pos_pbest[0] = pos_curr[0] 
     pos_pbest[1] = pos_curr[1] 
@@ -42,8 +38,9 @@ for i in 1..n
 end
 
 for j in 1..t 
-    file = File.open("data/sphere/data#{j}.txt", 'wb+')
+#    file = File.open("data/sphere/data#{j}.txt", 'wb+')
 #    file = File.open("data/rastrigin/data#{j}.txt", 'wb+')
+    file = File.open("data/fn/data#{j}.txt", 'wb+')
     for i in 1..n
         # update v
         # v = c1*r1*(pos_pbest-pos_curr) + c2*r2*(pos_gbest-pos_curr)
@@ -56,8 +53,7 @@ for j in 1..t
         particle[i][1][1] = particle[i][1][1] + v[1]
     
         # update pos_pbest
-        z = sphere(pos_curr[0], pos_curr[1])
-#        z = rastrigin(pos_curr[0], pos_curr[1])
+        z = fn(pos_curr[0], pos_curr[1])
         if (z < particle[i][3])
             particle[i][2] = pos_curr
             particle[i][3] = z
@@ -66,10 +62,12 @@ for j in 1..t
         # update gbest
         if (z < z_gbest)
             pos_gbest = particle[i][1]
+            z_gbest = z
         end
     
         file << "#{particle[i][1][0]} #{particle[i][1][1]}\n"
-        
+
+        p "(i#{j},p#{i}) #{z_gbest} at #{pos_gbest}"
     end
     file.close
 end
