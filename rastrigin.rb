c738cf2e8be76ccaeb5742a6a9b0b25e2b781300
Monica Tu <monica.service@gmail.com>
2012-10-16 16:08:14 +0800

    Add sphere & rastrigin function.

---
+++ rastrigin.rb
@@ -0,0 +1,5 @@
+def fn(x, y)
+   result = (x**2-10*Math.cos(2*PI*x)+10) + (y**2-10*Math.cos(2*PI*y)+10) 
+   return result
+end
+
---
+++ sphere.rb
@@ -0,0 +1,4 @@
+def fn(x, y)
+   result = x**2 + y**2 
+   return result
+end
