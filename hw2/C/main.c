f22de5b3d67ef572824a4d0bc2304ab97b76ae93
Monica Tu <monica.service@gmail.com>
2012-11-16 22:51:56 +0800

    (hw2, main) Stop when var < 0.0000000001.

--- hw2/C/main.c
+++ hw2/C/main.c
@@ -6,40 +6,24 @@ int main(int argc, char **argv)
 
     srand((unsigned)time(NULL));
 
-/*for (i = 0; i< 10; i++)*/
-/*          printf("rand: %d\n", rand());*/
-#if 1
     initialize();
-/*    print_particles();*/
     for(i=0; i<ITERA_CNT; i++){
-/*        print_particles();*/
         selection();
-/*        print_particles();*/
 
-/*  print_particles();*/
         crossover();
-/*  print_particles();*/
-
-/*        print_particles();*/
-
         mutation();
-/*        print_particles();*/
 
         printf("\n(%3d itera) ", i);
 
-/*        print_particles();*/
         float mean = cal_mean();
-        printf(" || mean: %12.10lf; ", mean);
+        printf(" || mean: %10.10lf;\t", mean);
 
         float var = cal_var();
-        printf(" || var: %12.10lf; ", var);
-
-/*        print_particles();*/
-/*        print_particles();*/
-/*        print_ever_best();*/
-/*getchar();*/
+        printf(" || var: %10.10lf;", var);
+        if ( var < 0.0000000001 ) {
+          break;
+        }
     }
     printf("\n");
-#endif
     return 0;
 }
